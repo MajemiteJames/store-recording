6db8877a29952be1aa2bb3cfd5a1dd3d97e58606
MajemiteJames <majemiteokoro@gmail.com>
2019-06-16 04:56:30 +0100

    functionality of removed item

--- src/context.js
+++ src/context.js
@@ -73,7 +73,25 @@ import { template } from '@babel/core';
         console.log("this is decrement method");
     };
     removeItem = (id) => {
-        console.log("removed item method")
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() => {
+            return {
+                cart:[...tempCart],
+                products:[...tempProducts],
+            }
+        },() =>{
+            this.addTotals();
+        })
     };
     clearCart = ( ) => {
        this.setState(() => {
